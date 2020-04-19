5f496098b54ba27ee8040cb8282bf363b4e85afb
chuxmykel <ngwobiachukwudi@gmail.com>
2020-04-19 13:39:20 +0100

    fix(CI): fix migration file with dummy up & down functions

--- src/database/migrations/index.js
+++ src/database/migrations/index.js
@@ -1,2 +1,11 @@
 // Dummy file to enable Travis test pass
 // TODO: Delete this file
+
+/* eslint-disable require-jsdoc */
+export const up = () => {
+  // TODO: Delete
+};
+
+export const down = () => {
+  // TODO: Delete
+};
