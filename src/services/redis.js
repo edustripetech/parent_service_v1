36ae8cbc7b086fc7e3b61a3fee7416ab2b3ee0cb
Samuel Oyegbemi <samueloyegbemi@gmail.com>
2020-05-17 23:21:45 +0100

    Added frequently used function in redis

--- src/services/redis.js
+++ src/services/redis.js
@@ -17,4 +17,60 @@ redisClient.on('error', (err) => {
   if (!isProduction) debug(`[app] ${chalk.red(err)}`);
 });
 
+/**
+ * @name getKey
+ * @param {Object} req Express request object
+ * @param {Boolean} withUserId Value indicating whether to include userId while generating the key. The Id is gotten from req object which must has been set with jwt middleware
+ * @return {string} Generated key
+ */
+export const getKey = (req, withUserId = false) => {
+  const { method, url } = req;
+  let key = `method:${method}, url:${url}`;
+  if (req.userId && withUserId) key = `user:${req.userId}, ${key}`;
+  return key;
+};
+
+/**
+ * @name readFromCache
+ * @param {Object} req Express request object
+ * @param {Function} callback Callback function to receive the retrieved data
+ * @param {Boolean} withUserId
+ * @return {Boolean} Returns if data exits in cache or not
+ */
+export const readFromCache = (req, callback, withUserId = false) => {
+  const key = getKey(req, withUserId);
+  redisClient.get(key, (err, data) => {
+    let cachedObj;
+    if (data) {
+      try {
+        cachedObj = JSON.parse(data);
+      } catch (e) {
+        cachedObj = data;
+      }
+    }
+    callback(cachedObj);
+  });
+};
+
+/**
+ * @name setCache
+ * @param {Object} req Express request object
+ * @param {Object} data Data to be cached
+ * @param {Number} expiresIn expiration time for cache in seconds
+ * @param {Boolean} withUserId Boolean value specifying if the cache belongs to a specific user
+ * @param {Function} callback optional function for successful cache
+ * @return {Null} Null
+ */
+export const setCache = (
+  req,
+  data,
+  expiresIn = null,
+  withUserId = false,
+  callback = () => {},
+) => {
+  const exp = expiresIn || process.env.REDIS_DATA_EXPIRATION;
+  const key = getKey(req, withUserId);
+  redisClient.setex(key, exp, JSON.stringify(data), callback);
+};
+
 export default redisClient;
